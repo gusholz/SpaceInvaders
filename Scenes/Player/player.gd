b84cc94280bc24842037528ea09d5b814081b371
Gustavo Holzmann <gustavobarretohv@live.com>
2023-10-13 13:40:39 -0300

    Created the map and the player, with the movement script

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,18 +1,30 @@
 extends CharacterBody2D
 
 @export var speed = 80
+const SPRITE_SIZE = 7
+const LEFT_BORDER = 0 + SPRITE_SIZE
+const RIGHT_BORDER = 384 - SPRITE_SIZE  # Adjust this value according to your needs
 
 func _process(delta):
 	move_player()
 
 func move_player():
 	var movementVector = Vector2()
-	
-	# Check for left and right input
-	if Input.is_action_pressed("move_left"):
+
+	if Input.is_action_pressed("move_left") && check_Borders(movementVector.x, "left"):
 		movementVector.x -= 1
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right") && check_Borders(movementVector.x, "right"):
 		movementVector.x += 1
 
 	velocity = movementVector.normalized() * speed
 	move_and_slide()
+
+func check_Borders(number: float, direction: String) -> bool:
+	var character_position = get_global_transform().origin.x
+
+	if direction == "left":
+		return character_position + number >= LEFT_BORDER
+	elif direction == "right":
+		return character_position + number <= RIGHT_BORDER
+	else:
+		return true
